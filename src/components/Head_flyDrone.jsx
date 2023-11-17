35a2cdb1cdef3c475ac6e9de4724fe2132c0c8ae
Franco Canavese <fcanavese@hotmail.com>
2023-11-17 20:51:34 +0100

    transition_menu

--- src/components/Head_flyDrone.jsx
+++ src/components/Head_flyDrone.jsx
@@ -6,39 +6,36 @@ import imagenPortada from '../assets/img_2.png';
 function Head_flyDrone() {
 
   return (
-    <>
-    <div className='portada'>
-      <img src={imagenPortada} alt="imagen_portada" />
-    </div>
+    <div className='contenedor'>
 
-    <div className='head'>
-
-      <div className='fly_drone'>
-        Fly Drone
+      <div className='portada'>
+        <img src={imagenPortada} alt="imagen_portada" />
       </div>
 
-      <div className='contenedor_menu'>
+      <div className='head'>
+        <div className='fly_drone'>
+          Fly Drone
+        </div>
 
-        <ul className='ul_menu'>          
-          <Link to="/" className='link'>
-            <li className='li_menu'>Inicio</li>
-          </Link>
-          <Link to="/productos" className='link'>
-            <li className='li_menu'>Productos</li>
-          </Link>  
-          <Link to="/contactanos" className='link'>
-            <li className='li_menu'>Contactanos</li>
-          </Link>  
-          <Link to="/acerca_de" className='link'>
-            <li className='li_menu'>Acerca de</li>
-          </Link>      
-        </ul>
-
+        <div className='contenedor_menu'>
+          <ul className='ul_menu'>          
+            <Link to="/" className='link'>
+              <li className='li_menu'>Inicio</li>
+            </Link>
+            <Link to="/productos" className='link'>
+              <li className='li_menu'>Productos</li>
+            </Link>  
+            <Link to="/contactanos" className='link'>
+              <li className='li_menu'>Contactanos</li>
+            </Link>  
+            <Link to="/acerca_de" className='link'>
+              <li className='li_menu'>Acerca de</li>
+            </Link>      
+          </ul>
+        </div>
       </div>
 
     </div>
-
-    </>
   )
 }
 
